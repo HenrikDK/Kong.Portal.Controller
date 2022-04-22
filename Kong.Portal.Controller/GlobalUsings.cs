6e49078c91eb5c4a019f3bba57e4290c90e2d5dd
HenrikDK <commander.john.crichton@gmail.com>
2022-04-22 02:07:32 +0200

    add global usings

---
+++ Kong.Portal.Controller/GlobalUsings.cs
@@ -0,0 +1,19 @@
+global using System;
+global using System.Diagnostics;
+global using System.IO;
+global using System.Linq;
+global using System.Collections.Generic;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+
+global using Lamar;
+global using Lamar.Microsoft.DependencyInjection;
+global using MoreLinq.Extensions;
+global using Prometheus;
+global using Serilog;
+global using Serilog.Formatting.Json;
