b2c7e3019f705612ccdc094685a205f183a910b7
HenrikDK <commander.john.crichton@gmail.com>
2022-04-25 01:23:48 +0200

    workload scaffolding

---
+++ Kong.Portal.Controller/Setup/ApiReconciliation.cs
@@ -0,0 +1,28 @@
+namespace Kong.Portal.Controller.Setup;
+
+public interface IApiReconciliation
+{
+    void ProcessClusterApis();
+}
+    
+public class ApiReconciliation : IApiReconciliation
+{
+    private readonly ILogger<ApiReconciliation> _logger;
+
+    public ApiReconciliation(ILogger<ApiReconciliation> logger)
+    {
+        _logger = logger;
+    }
+       
+    public void ProcessClusterApis()
+    {
+        try
+        {
+            _logger.LogInformation($"Updating..");
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error processing cluster apis");
+        }
+    }
+}
---
+++ Kong.Portal.Controller/Setup/ReconciliationScheduler.cs
@@ -0,0 +1,44 @@
+namespace Kong.Portal.Controller.Setup;
+
+public interface IReconciliationScheduler
+{
+    void RunOnInterval(CancellationToken token, TimeSpan delay);
+}
+    
+public class ReconciliationScheduler : IReconciliationScheduler
+{
+    private readonly IApiReconciliation _apiReconciliation;
+    private TimeSpan _delay = TimeSpan.FromMinutes(1);
+    
+    public ReconciliationScheduler(IApiReconciliation apiReconciliation)
+    {
+        _apiReconciliation = apiReconciliation;
+    }
+    
+    public void RunOnInterval(CancellationToken token, TimeSpan delay)
+    {
+        _delay = delay;
+        var stopWatch = new Stopwatch();
+        while (!token.IsCancellationRequested)
+        {
+            stopWatch.Start();
+            
+            _apiReconciliation.ProcessClusterApis();
+
+            WaitForNextExecution(token, stopWatch);
+        }
+    }
+
+    private void WaitForNextExecution(CancellationToken token, Stopwatch stopWatch)
+    {
+        stopWatch.Stop();
+        var elapsed = stopWatch.Elapsed;
+        var calculated = _delay.Subtract(elapsed);
+        stopWatch.Reset();
+
+        if (elapsed < _delay)
+        {
+            Task.Delay(calculated, token).Wait(token);
+        }
+    }
+}
