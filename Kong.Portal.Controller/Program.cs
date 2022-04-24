0dc5d4ab023fa6ddf4909f81062e8102228f4a99
HenrikDK <commander.john.crichton@gmail.com>
2022-04-24 22:40:19 +0200

    k8s compatible flurl setup

--- Kong.Portal.Controller/Program.cs
+++ Kong.Portal.Controller/Program.cs
@@ -6,6 +6,19 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.Console(new JsonFormatter())
     .CreateLogger();
 
+var config = KubernetesClientConfiguration.IsInCluster()
+    ? KubernetesClientConfiguration.InClusterConfig()
+    : KubernetesClientConfiguration.BuildDefaultConfig();
+
+FlurlHttp.ConfigureClient(config.Host, cli => cli.Settings.HttpClientFactory = new UntrustedHttpsClientFactory());
+FlurlHttp.Configure(c =>
+{
+    c.JsonSerializer = new NewtonsoftJsonSerializer(new JsonSerializerSettings
+    {
+        ContractResolver = new CamelCasePropertyNamesContractResolver()
+    });
+});
+
 var builder = Host.CreateDefaultBuilder()
     .UseLamar((context, registry) => registry.Scan(x =>
     {
