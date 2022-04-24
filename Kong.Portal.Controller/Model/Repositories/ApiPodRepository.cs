e99b98347b53bda9b67088837942906b9d3ff5e5
HenrikDK <commander.john.crichton@gmail.com>
2022-04-25 00:30:06 +0200

    work on api pod repository

---
+++ Kong.Portal.Controller/Model/Repositories/ApiPodRepository.cs
@@ -0,0 +1,50 @@
+namespace Kong.Portal.Controller.Model.Repositories;
+
+public interface IApiPodRepository
+{
+    IList<ApiPod> GetAll();
+}
+    
+public class ApiPodRepository : IApiPodRepository
+{
+    private Lazy<KubernetesClientConfiguration> _config;
+
+    public ApiPodRepository()
+    {
+        _config = new Lazy<KubernetesClientConfiguration>(() => KubernetesClientConfiguration.IsInCluster()
+            ? KubernetesClientConfiguration.InClusterConfig()
+            : KubernetesClientConfiguration.BuildDefaultConfig());
+    }
+
+    public IList<ApiPod> GetAll()
+    {
+        var host = _config.Value.Host;
+
+        var pods = host.AppendPathSegment("/api/v1/pods")
+            .WithOAuthBearerToken(_config.Value.AccessToken)
+            .GetJsonAsync().Result;
+
+        var apiPods = new List<ApiPod>();
+        foreach (var pod in pods.items)
+        {
+            var name = pod.metadata.name.ToString();
+            if (!name.Contains("-api")) continue;
+
+            if (name.Contains("api-"))
+                name = name.Substring(0, name.IndexOf("-api")+4);
+            
+            apiPods.Add(new ApiPod
+            {
+                Name = name,
+                NameSpace = (string)((IDictionary<string, object>)pod.metadata)["namespace"],
+                LastUpdated = pod.metadata.creationTimestamp,
+            });
+        }
+
+        var lookup = apiPods.ToLookup(x => new {x.Name, x.NameSpace});
+        
+        var result = lookup.Select(x => x.OrderByDescending(x => x.LastUpdated).First()).ToList();
+
+        return result;
+    }
+}
