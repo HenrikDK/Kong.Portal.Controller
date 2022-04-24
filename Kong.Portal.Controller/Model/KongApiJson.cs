03cbd53cfcc2e91e1aa6722c334dbcd0bc9afa9f
HenrikDK <commander.john.crichton@gmail.com>
2022-04-25 00:29:44 +0200

    change field

--- Kong.Portal.Controller/Model/KongApiJson.cs
+++ Kong.Portal.Controller/Model/KongApiJson.cs
@@ -5,6 +5,7 @@ public class KongApiJson
     public string Name { get; set; }
     public string NameSpace { get; set; }
 
-    public DateTime? Updated { get; set; }
+    public DateTime Updated { get; set; }
     public string Json { get; set; }
+    public int MergedCount { get; set; }
 }
