d6a61a520c076f2decb802e015e396e25bbaa489
HenrikDK <commander.john.crichton@gmail.com>
2022-04-27 01:29:21 +0200

    add swagger endpoint repository

---
+++ Kong.Portal.Controller/Model/Repositories/ApiSwaggerRepository.cs
@@ -0,0 +1,32 @@
+namespace Kong.Portal.Controller.Model.Repositories;
+
+public interface IApiSwaggerRepository
+{
+    string GetSwaggerJson(KongApi api);
+}
+    
+public class ApiSwaggerRepository : IApiSwaggerRepository
+{
+    private readonly IConfiguration _configuration;
+    private Lazy<bool> _inCluster;
+
+    public ApiSwaggerRepository(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        _inCluster = new Lazy<bool>(() => KubernetesClientConfiguration.IsInCluster());
+    }
+
+    public string GetSwaggerJson(KongApi api)
+    {
+        var prefix = _configuration.GetValue<string>("ingress-prefix");
+        var host = _inCluster.Value
+            ? $"http://{api.Name}.{api.NameSpace}:{api.Port}{api.Swagger}"
+            : $"https://{api.Name.Substring(0, api.Name.Length - 4)}.{api.NameSpace}.{prefix}{api.Swagger}";
+        
+        var swagger = host
+            .AllowHttpStatus("4xx")
+            .GetStringAsync().Result;
+        
+        return swagger;
+    }
+}
