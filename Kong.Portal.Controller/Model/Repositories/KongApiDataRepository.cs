ae64870b1b69639c943cce599bfbb8c22e49e632
HenrikDK <commander.john.crichton@gmail.com>
2022-04-27 01:50:20 +0200

    extend api repository

--- Kong.Portal.Controller/Model/Repositories/KongApiDataRepository.cs
+++ Kong.Portal.Controller/Model/Repositories/KongApiDataRepository.cs
@@ -2,6 +2,7 @@ namespace Kong.Portal.Controller.Model.Repositories;
 
 public interface IKongApiDataRepository
 {
+    IList<KongApiData> GetAll();
     IList<KongApiData> GetAll(string nameSpace);
     void Delete(KongApiData data);
     void Insert(KongApiData data);
@@ -16,6 +17,29 @@ public class KongApiDataRepository : IKongApiDataRepository
         _config = new Lazy<KubernetesClientConfiguration>(() => KubernetesClientConfiguration.IsInCluster()
             ? KubernetesClientConfiguration.InClusterConfig()
             : KubernetesClientConfiguration.BuildDefaultConfig());
+    }
+    
+    public IList<KongApiData> GetAll()
+    {
+        var host = _config.Value.Host;
+
+        var apis = host.AppendPathSegment($"/apis/henrik.dk/v1/kong-api-data")
+            .WithOAuthBearerToken(_config.Value.AccessToken)
+            .GetJsonAsync().Result;
+
+        var result = new List<KongApiData>();
+        foreach (var api in apis.items)
+        {
+            result.Add(new KongApiData
+            {
+                Name = api.metadata.name,
+                NameSpace = (string)((IDictionary<string, object>)api.metadata)["namespace"],
+                Data = api.spec.json,
+                Updated = api.spec.updated,
+            });
+        }
+
+        return result;
     }
     
     public IList<KongApiData> GetAll(string nameSpace)
