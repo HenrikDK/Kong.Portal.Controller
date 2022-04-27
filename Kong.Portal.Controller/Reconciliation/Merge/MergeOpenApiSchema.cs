05a9798994780456dfaac84f7ac02237ffd28f4a
HenrikDK <commander.john.crichton@gmail.com>
2022-04-27 02:17:30 +0200

    port of merge logic from merge open api

---
+++ Kong.Portal.Controller/Reconciliation/Merge/MergeOpenApiSchema.cs
@@ -0,0 +1,48 @@
+using Flurl.Util;
+using Kong.Portal.Controller.Model;
+using Kong.Portal.Controller.Model.Extensions;
+using Kong.Portal.Controller.Model.Repositories;
+using Newtonsoft.Json.Linq;
+
+namespace Kong.Portal.Controller.Reconciliation.Merge;
+
+public interface IMergeOpenApiSchema
+{
+    void Execute(JObject mainSchema, KongApiData api);
+}
+    
+public class MergeOpenApiSchema : IMergeOpenApiSchema
+{
+    private readonly IConfiguration _configuration;
+    private readonly IKongApiConfigRepository _kongApiConfigRepository;
+
+    public MergeOpenApiSchema(IConfiguration configuration, IKongApiConfigRepository kongApiConfigRepository)
+    {
+        _configuration = configuration;
+        _kongApiConfigRepository = kongApiConfigRepository;
+    }
+        
+    public void Execute(JObject mainSchema, KongApiData api)
+    {
+        var schema = api.Data.FromBrotliBase64(); 
+        var json = JObject.Parse(schema);
+
+        var types = json["components"]["schemas"].ToKeyValuePairs();
+        foreach (var type in types)
+        {
+            schema = schema.Replace($"#/components/schemas/{type.Key}", $"#/components/schemas/{api.Name}_{type.Key}");
+        }
+
+        var serviceJson = JObject.Parse(schema);
+        foreach (var type in types)
+        {
+            mainSchema["components"]["schemas"][$"{api.Name}_{type.Key}"] = serviceJson["components"]["schemas"][type.Key];
+        }
+
+        var paths = json["paths"].ToKeyValuePairs();
+        foreach (var path in paths)
+        {
+            mainSchema["paths"][path.Key] = serviceJson["paths"][path.Key];
+        }
+    }
+}
