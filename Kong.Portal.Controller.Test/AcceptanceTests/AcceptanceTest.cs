be0cc234678dcbd4e796acd65925db85995ae893
HenrikDK <commander.john.crichton@gmail.com>
2022-04-28 00:27:46 +0200

    update base test case

--- Kong.Portal.Controller.Test/AcceptanceTests/AcceptanceTest.cs
+++ Kong.Portal.Controller.Test/AcceptanceTests/AcceptanceTest.cs
@@ -1,5 +1,7 @@
 using Kong.Portal.Controller.Infrastructure;
-using Microsoft.AspNetCore.Connections;
+using Kong.Portal.Controller.Reconciliation;
+using Kong.Portal.Controller.Reconciliation.Merge;
+using Kong.Portal.Controller.Reconciliation.Update;
 
 namespace Kong.Portal.Controller.Test.AcceptanceTests;
 
@@ -9,19 +11,38 @@ public class AcceptanceTest
 
     protected WorkerRegistry _registry;
     protected Container _container;
+    private IConfiguration _configuration;
 
     public AcceptanceTest()
     {
         _registry = new WorkerRegistry();
+        _registry.Scan(x =>
+        {
+            x.AssemblyContainingType<ServiceHost>();
+            x.WithDefaultConventions();
+            x.LookForRegistries();
+        });
 
+        MockConfiguration();
         MockLogging();
         MockMemoryCache();
     }
-    
+
+    private void MockConfiguration()
+    {
+        _configuration = Substitute.For<IConfiguration>();
+        _registry.AddSingleton(_configuration);
+    }
+
     private void MockLogging()
     {
         _registry.AddSingleton(Substitute.For<ILogger>());
         _registry.AddSingleton(Substitute.For<ILogger<ServiceHost>>());
+        _registry.AddSingleton(Substitute.For<ILogger<ReconciliationScheduler>>());
+        _registry.AddSingleton(Substitute.For<ILogger<ApiReconciliation>>());
+        _registry.AddSingleton(Substitute.For<ILogger<UpdateClusterApis>>());
+        _registry.AddSingleton(Substitute.For<ILogger<MergeClusterApis>>());
+        _registry.AddSingleton(Substitute.For<ILogger<MergeOpenApiSchemas>>());
     }
         
     private void MockMemoryCache()
