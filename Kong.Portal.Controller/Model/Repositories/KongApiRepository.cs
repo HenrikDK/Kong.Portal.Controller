4a8dfc87969d56ab45e412b668854cda2d28c9cf
HenrikDK <commander.john.crichton@gmail.com>
2022-04-28 22:39:22 +0200

    expand kong api repository

--- Kong.Portal.Controller/Model/Repositories/KongApiRepository.cs
+++ Kong.Portal.Controller/Model/Repositories/KongApiRepository.cs
@@ -3,6 +3,7 @@ namespace Kong.Portal.Controller.Model.Repositories;
 public interface IKongApiRepository
 {
     IList<KongApi> GetAll();
+    IList<KongApi> GetAll(string nameSpace);
 }
     
 public class KongApiRepository : IKongApiRepository
@@ -21,6 +22,29 @@ public class KongApiRepository : IKongApiRepository
         var host = _config.Value.Host;
 
         var pods = host.AppendPathSegment("/apis/henrik.dk/v1/kong-apis")
+            .WithOAuthBearerToken(_config.Value.AccessToken)
+            .GetJsonAsync().Result;
+
+        var apis = new List<KongApi>();
+        foreach (var api in pods.items)
+        {
+            apis.Add(new KongApi
+            {
+                Name = api.metadata.name,
+                NameSpace = (string)((IDictionary<string, object>)api.metadata)["namespace"],
+                Port = (int) api.spec.port,
+                Swagger = api.spec.swagger
+            });
+        }
+
+        return apis;
+    }
+    
+    public IList<KongApi> GetAll(string nameSpace)
+    {
+        var host = _config.Value.Host;
+
+        var pods = host.AppendPathSegment($"/apis/henrik.dk/v1/namespaces/{nameSpace}/kong-apis")
             .WithOAuthBearerToken(_config.Value.AccessToken)
             .GetJsonAsync().Result;
 
