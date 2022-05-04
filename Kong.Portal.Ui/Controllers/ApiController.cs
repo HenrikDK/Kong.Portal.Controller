8c15801e01eb3dd290dd80cc3476868621c59f96
HenrikDK <commander.john.crichton@gmail.com>
2022-05-05 00:31:24 +0200

    rework controller

---
+++ Kong.Portal.Ui/Controllers/ApiController.cs
@@ -0,0 +1,26 @@
+﻿using Kong.Portal.Ui.Model;
+
+namespace Kong.Portal.Ui.Controllers;
+
+[ApiController]
+public class ApiController : ControllerBase
+{
+    private readonly ICachedSchemaRepository _cache;
+
+    public ApiController(ICachedSchemaRepository cache)
+    {
+        _cache = cache;
+    }
+        
+    [HttpGet("/swagger.json")]
+    public ActionResult Get()
+    {
+        var json = _cache.GetSchema();
+        if (json == null)
+        {
+            return NotFound();
+        }
+            
+        return Content(json, "application/json");
+    }
+}
